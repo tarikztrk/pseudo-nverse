7c802ae9fb633c14d7889fd838630ecf5ef6d1cb
tarikztrk <tarik7337@gmail.com>
2019-01-04 20:04:34 +0300

    Add files via upload

---
+++ proje2yazlab/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace proje2yazlab
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(272, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "satır";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "sutun";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "matris olustur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(409, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "rastgele değer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(626, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 32);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "pseudo inverse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(50, 168);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(551, 291);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(689, 328);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 32);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "ters";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(689, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 32);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "carp";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(689, 168);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(109, 32);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "transpoz al";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(689, 377);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(109, 32);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "çarp";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 675);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ proje2yazlab/Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje2yazlab
+{
+    public partial class Form1 : Form
+    {
+        double determinant;
+        double[,] matris3;
+        double[,] matris1;
+        double[,] sonucMatrisi;
+        int satir;
+        int sutun;
+        private TextBox[,] matris2;
+        private TextBox[,] temp4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            groupBox1.Controls.Clear();
+            if (!int.TryParse(textBox1.Text, out satir))
+            {
+                MessageBox.Show("Satır sayısı boş bırakılamaz.");
+                return;
+            }
+            if (!int.TryParse(textBox2.Text, out sutun))
+            {
+                MessageBox.Show("Sutun sayısı boş bırakılamaz.");
+                return;
+            }
+           
+            matris2 = new TextBox[satir, sutun];
+            int boyut = groupBox1.Width / sutun;
+            for (int i = 0; i < matris2.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris2.GetLength(1); j++)
+                {
+                    matris2[i, j] = new TextBox();
+                    matris2[i, j].Text = "0";
+                    matris2[i, j].Top = (i * matris2[i, j].Height) + 20;
+                    matris2[i, j].Left = j * boyut + 6;
+                    matris2[i, j].Width = boyut;
+                    groupBox1.Controls.Add(matris2[i, j]);
+                }
+            }
+
+        }
+        Random rastgele = new Random();
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            groupBox1.Controls.Clear();
+            if (!int.TryParse(textBox1.Text, out satir))
+            {
+                MessageBox.Show("Satır sayısı boş bırakılamaz.");
+                return;
+            }
+            if (!int.TryParse(textBox2.Text, out sutun))
+            {
+                MessageBox.Show("Sutun sayısı boş bırakılamaz.");
+                return;
+            }
+            
+           matris2 = new TextBox[satir, sutun];
+            int boyut = groupBox1.Width / sutun;
+            for (int i = 0; i < matris2.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris2.GetLength(1); j++)
+                {
+                    matris2[i, j] = new TextBox();
+                    double sayi = rastgele.Next(9);
+                    matris2[i, j].Text = sayi.ToString();
+                    matris2[i, j].Top = (i * matris2[i, j].Height) + 20;
+                    matris2[i, j].Left = j * boyut + 6;
+                    matris2[i, j].Width = boyut;
+                    groupBox1.Controls.Add(matris2[i, j]);
+                    
+                }
+            }
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double[,] ozdegerMatrisi = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            double[,] kofaktor = new double[matris2.GetLength(0), matris2.GetLength(1)];
+
+            double[,] temp = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            for (int x = 0; x < matris2.GetLength(0); x++)
+            {
+                for (int y = 0; y < matris2.GetLength(1); y++)
+                {
+                    float n = 0;
+                    float.TryParse(matris2[x, y].Text, out n);
+                    temp[x, y] = n;
+                   
+                }
+            }
+            double[,] sonuc = matematikselİslemler.transpoze(temp);
+            double[,] temp2 = matematikselİslemler.carpma(sonuc, temp);
+
+            
+
+            ////double[,] temp = matematikselİslemler.carpma(matematikselİslemler.transpoze(matris1), matris1);
+            if (temp2.GetLength(0) == 2 && temp2.GetLength(1) == 2)
+            {
+                determinant = matematikselİslemler.determinantAl2x2(temp2);
+            }
+            if (temp2.GetLength(0) == 3 && temp2.GetLength(1) == 3)
+            {
+                determinant = matematikselİslemler.determinantAl3x3(temp2);
+            }
+
+            if (temp2.GetLength(0) == 2 && temp2.GetLength(1) == 2)
+            {
+                kofaktor = matematikselİslemler.kofaktorAl2x2(temp2);
+                ozdegerMatrisi = matematikselİslemler.transpoze(kofaktor);
+            }
+            if (temp2.GetLength(0) == 3 && temp2.GetLength(1) == 3)
+            {
+                kofaktor = matematikselİslemler.kofaktorAl3x3(temp2);
+                ozdegerMatrisi = matematikselİslemler.transpoze(kofaktor);
+            }
+
+
+
+            double[,] temp3 = matematikselİslemler.tersAl(determinant,ozdegerMatrisi);
+
+            double[,] tempMatris4 = matematikselİslemler.carpma(temp3, sonuc);
+
+            ////sonucMatrisi = temp;
+            temp4 = new TextBox[tempMatris4.GetLength(0), tempMatris4.GetLength(1)];
+            int boyut = groupBox1.Width / temp4.GetLength(1);
+            groupBox1.Controls.Clear();
+            for (int i = 0; i < temp4.GetLength(0); i++)
+            {
+                for (int j = 0; j < temp4.GetLength(1); j++)
+                {
+                    temp4[i, j] = new TextBox();
+
+                    temp4[i, j].Text = tempMatris4[i, j].ToString();
+                    temp4[i, j].Top = (i * temp4[i, j].Height) + 20;
+                    temp4[i, j].Left = j * boyut + 6;
+                    temp4[i, j].Width = boyut;
+                    groupBox1.Controls.Add(temp4[i, j]);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double[,] temp = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            for (int x = 0; x < matris2.GetLength(0); x++)
+            {
+                for (int y = 0; y < matris2.GetLength(1); y++)
+                {
+                    float n = 0;
+                    float.TryParse(matris2[x, y].Text, out n);
+                    temp[x, y] = n;
+
+                }
+            }
+            double[,] sonuc = matematikselİslemler.transpoze(temp);
+
+            temp4 = new TextBox[sonuc.GetLength(0), sonuc.GetLength(1)];
+            int boyut = groupBox1.Width / temp4.GetLength(1);
+            groupBox1.Controls.Clear();
+            for (int i = 0; i < temp4.GetLength(0); i++)
+            {
+                for (int j = 0; j < temp4.GetLength(1); j++)
+                {
+                    temp4[i, j] = new TextBox();
+
+                    temp4[i, j].Text = sonuc[i, j].ToString();
+                    temp4[i, j].Top = (i * temp4[i, j].Height) + 20;
+                    temp4[i, j].Left = j * boyut + 6;
+                    temp4[i, j].Width = boyut;
+                    groupBox1.Controls.Add(temp4[i, j]);
+                }
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double[,] temp = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            for (int x = 0; x < matris2.GetLength(0); x++)
+            {
+                for (int y = 0; y < matris2.GetLength(1); y++)
+                {
+                    float n = 0;
+                    float.TryParse(matris2[x, y].Text, out n);
+                    temp[x, y] = n;
+
+                }
+            }
+            double[,] sonuc = matematikselİslemler.transpoze(temp);
+            double[,] temp2 = matematikselİslemler.carpma(sonuc, temp);
+
+
+            temp4 = new TextBox[temp2.GetLength(0), temp2.GetLength(1)];
+            int boyut = groupBox1.Width / temp4.GetLength(1);
+            groupBox1.Controls.Clear();
+            for (int i = 0; i < temp4.GetLength(0); i++)
+            {
+                for (int j = 0; j < temp4.GetLength(1); j++)
+                {
+                    temp4[i, j] = new TextBox();
+
+                    temp4[i, j].Text = temp2[i, j].ToString();
+                    temp4[i, j].Top = (i * temp4[i, j].Height) + 20;
+                    temp4[i, j].Left = j * boyut + 6;
+                    temp4[i, j].Width = boyut;
+                    groupBox1.Controls.Add(temp4[i, j]);
+                }
+            }
+        }
+
+       
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double[,] ozdegerMatrisi = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            double[,] kofaktor = new double[matris2.GetLength(0), matris2.GetLength(1)];
+
+            double[,] temp = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            for (int x = 0; x < matris2.GetLength(0); x++)
+            {
+                for (int y = 0; y < matris2.GetLength(1); y++)
+                {
+                    float n = 0;
+                    float.TryParse(matris2[x, y].Text, out n);
+                    temp[x, y] = n;
+
+                }
+            }
+            double[,] sonuc = matematikselİslemler.transpoze(temp);
+            double[,] temp2 = matematikselİslemler.carpma(sonuc, temp);
+
+
+
+            ////double[,] temp = matematikselİslemler.carpma(matematikselİslemler.transpoze(matris1), matris1);
+            if (temp2.GetLength(0) == 2 && temp2.GetLength(1) == 2)
+            {
+                determinant = matematikselİslemler.determinantAl2x2(temp2);
+            }
+            if (temp2.GetLength(0) == 3 && temp2.GetLength(1) == 3)
+            {
+                determinant = matematikselİslemler.determinantAl3x3(temp2);
+            }
+
+            if (temp2.GetLength(0) == 2 && temp2.GetLength(1) == 2)
+            {
+                kofaktor = matematikselİslemler.kofaktorAl2x2(temp2);
+                ozdegerMatrisi = matematikselİslemler.transpoze(kofaktor);
+            }
+            if (temp2.GetLength(0) == 3 && temp2.GetLength(1) == 3)
+            {
+                kofaktor = matematikselİslemler.kofaktorAl3x3(temp2);
+                ozdegerMatrisi = matematikselİslemler.transpoze(kofaktor);
+            }
+
+
+
+            double[,] temp3 = matematikselİslemler.tersAl(determinant, ozdegerMatrisi);
+
+            temp4 = new TextBox[temp3.GetLength(0), temp3.GetLength(1)];
+            int boyut = groupBox1.Width / temp4.GetLength(1);
+            groupBox1.Controls.Clear();
+            for (int i = 0; i < temp4.GetLength(0); i++)
+            {
+                for (int j = 0; j < temp4.GetLength(1); j++)
+                {
+                    temp4[i, j] = new TextBox();
+
+                    temp4[i, j].Text = temp3[i, j].ToString();
+                    temp4[i, j].Top = (i * temp4[i, j].Height) + 20;
+                    temp4[i, j].Left = j * boyut + 6;
+                    temp4[i, j].Width = boyut;
+                    groupBox1.Controls.Add(temp4[i, j]);
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {double[,] ozdegerMatrisi = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            double[,] kofaktor = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            double[,] temp = new double[matris2.GetLength(0), matris2.GetLength(1)];
+            for (int x = 0; x < matris2.GetLength(0); x++)
+            {
+                for (int y = 0; y < matris2.GetLength(1); y++)
+                {
+                    float n = 0;
+                    float.TryParse(matris2[x, y].Text, out n);
+                    temp[x, y] = n;
+
+                }
+            }
+            double[,] sonuc = matematikselİslemler.transpoze(temp);
+            double[,] temp2 = matematikselİslemler.carpma(sonuc, temp);
+
+
+
+            ////double[,] temp = matematikselİslemler.carpma(matematikselİslemler.transpoze(matris1), matris1);
+            if (temp2.GetLength(0) == 2 && temp2.GetLength(1) == 2)
+            {
+                determinant = matematikselİslemler.determinantAl2x2(temp2);
+            }
+            if (temp2.GetLength(0) == 3 && temp2.GetLength(1) == 3)
+            {
+                determinant = matematikselİslemler.determinantAl3x3(temp2);
+            }
+
+            if (temp2.GetLength(0) == 2 && temp2.GetLength(1) == 2)
+            {
+                kofaktor = matematikselİslemler.kofaktorAl2x2(temp2);
+                ozdegerMatrisi = matematikselİslemler.transpoze(kofaktor);
+            }
+            if (temp2.GetLength(0) == 3 && temp2.GetLength(1) == 3)
+            {
+                kofaktor = matematikselİslemler.kofaktorAl3x3(temp2);
+                ozdegerMatrisi = matematikselİslemler.transpoze(kofaktor);
+            }
+
+
+
+            double[,] temp3 = matematikselİslemler.tersAl(determinant, ozdegerMatrisi);
+
+            double[,] tempMatris4 = matematikselİslemler.carpma(temp3, sonuc);
+
+            temp4 = new TextBox[tempMatris4.GetLength(0), tempMatris4.GetLength(1)];
+            int boyut = groupBox1.Width / temp4.GetLength(1);
+            groupBox1.Controls.Clear();
+            for (int i = 0; i < temp4.GetLength(0); i++)
+            {
+                for (int j = 0; j < temp4.GetLength(1); j++)
+                {
+                    temp4[i, j] = new TextBox();
+
+                    temp4[i, j].Text = tempMatris4[i, j].ToString();
+                    temp4[i, j].Top = (i * temp4[i, j].Height) + 20;
+                    temp4[i, j].Left = j * boyut + 6;
+                    temp4[i, j].Width = boyut;
+                    groupBox1.Controls.Add(temp4[i, j]);
+                }
+            }
+        }
+    }
+}
